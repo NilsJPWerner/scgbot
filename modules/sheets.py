d9220119c2e2037896765e903282291529e40373
Nils Werner <nils.jp.werner@gmail.com>
2017-02-16 12:09:41 -0600

    Added personal messages

--- modules/sheets.py
+++ modules/sheets.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -20,6 +21,11 @@ NEXT_CHALLENGE_NAME = 'B14'
 NEXT_CHALLENGE_COUNT = 'B15'
 NEXT_CHALLENGE_DATE = 'B16'
 
+# Personal infos
+USER_INFO_RANGE = 'E4:I39'
+USER_INFO_ROW_LEN = 5  # From E to I
+
+# Sign-up constants
 GIFT_COL = 'H'
 
 class OutreachSheet(object):
@@ -28,27 +34,24 @@ class OutreachSheet(object):
         GC = gspread.authorize(CREDENTIALS)
         self.bot_sheet = GC.open("2017 Committee Outreach").worksheet("bot_sheet")
 
-    def test_bot_sheet(self):
-        test_cell = self.bot_sheet.acell(TEST_CELL)
-        if test_cell.value == 'test':
-            print 'bot_sheet is working'
-        else:
-            print 'Something is wrong with bot_sheet'
-            print 'B3 contained: %s' % test_cell.value
-
-    def create_daily_update_message(self):
-        donations = self.get_total_donations()
-        total_goal = self.get_total_donations_goal()
+    def get_daily_update_message(self):
+        donations = int(self.bot_sheet.acell(TOTAL_DONATIONS_CELL).value)
+        total_goal = int(self.bot_sheet.acell(TOTAL_DONATION_GOAL_CELL).value)
         percentage = round((float(donations) / total_goal) * 100, 2)
 
         message = ":moneybag: *This is your daily giving update* :moneybag:\n\n\n"
         message += ":bar_chart: Total Current Donations: %d\n" % donations
-        message += ":chart_with_upwards_trend:"
+        message += ":chart_with_upwards_trend: "
         message += "At %0.2f%% of %d donor goal\n" % (percentage, total_goal)
 
-        challenge = self.get_next_challenge()
-        message += ":calendar: Next Challenge: %s - %s\n" % (challenge['name'], challenge['date'])
-        message += ":squirrel: %d donations left to go\n\n\n" % (challenge['count'] - donations)
+        try:
+            challenge = self.get_next_challenge()
+            message += ":calendar: Next Challenge: %s - " % challenge['name']
+            message += "%d donations\n" % challenge['count']
+            message += ":squirrel: %s days & " % (challenge['date'] - datetime.now()).days
+            message += "%d donations left to go\n\n\n" % (challenge['count'] - donations)
+        except ValueError:
+            message += '\n\n'
 
         leaderboard = self.get_leaderboard()
         message += ":sports_medal: Current Leaderboard :sports_medal:\n"
@@ -56,45 +59,25 @@ class OutreachSheet(object):
             message += "%s %s - %s\n" % (p[1], p[0][0], p[0][1])
         return message
 
-    def get_individual_update_message(self, user_id):
-        total_donations = self.get_user_donations_this_weeks(user_id)
-        weeks_donations = self.get_user_donations_this_weeks(user_id)
-        real_name = self.get_user_real_name(user_id)
-
-        message = ":moneybag: *%s here is your personal update* :moneybag:\n\n" % real_name
-        message += ":bar_chart: Your donations this week: %s\n" % weeks_donations
-        message += ":chart_with_upwards_trend: Your total donations: %s\n" % total_donations
-        return message
-
-    def get_total_donations(self):
-        return int(self.bot_sheet.acell(TOTAL_DONATIONS_CELL).value)
-
-    def get_total_donations_goal(self):
-        return int(self.bot_sheet.acell(TOTAL_DONATION_GOAL_CELL).value)
-
-    def user_id_to_name(self, user_id):
-        user_id_cell = self.bot_sheet.find(user_id)
-        user_name_cell = self.bot_sheet.cell(user_id_cell.row, user_id_cell.col - 1)
-        return user_name_cell.value
-
-    def get_user_total_donations(self, user_id):
-        user_id_cell = self.bot_sheet.find(user_id)
-        user_total_donation_cell = self.bot_sheet.cell(user_id_cell.row, user_id_cell.col + 1)
-        return int(user_total_donation_cell.value)
-
-    def get_user_donations_this_weeks(self, user_id):
-        user_id_cell = self.bot_sheet.find(user_id)
-        return self.bot_sheet.cell(user_id_cell.row, user_id_cell.col + 3).value
-
-    def get_user_real_name(self, user_id):
-        user_id_cell = self.bot_sheet.find(user_id)
-        return self.bot_sheet.cell(user_id_cell.row, user_id_cell.col - 1).value
+    def get_personal_update_messages(self):
+        members = self.get_all_members()
+        messages = []
+        for member in members:
+            text = ":moneybag: *%s here is your personal update* :moneybag:\n\n" % member['name']
+            text += ":bar_chart: Your donations this fortnight: %s\n" % member['current_fortnight']
+            text += ":chart_with_upwards_trend: Your total donations: %s\n" % member['total_donations']
+            messages.append({'slack_id': member['slack_id'], 'text': text})
+        return messages
 
     def get_next_challenge(self):
         challenge = {}
         challenge['name'] = self.bot_sheet.acell(NEXT_CHALLENGE_NAME).value
-        challenge['date'] = self.bot_sheet.acell(NEXT_CHALLENGE_DATE).value
+        date = self.bot_sheet.acell(NEXT_CHALLENGE_DATE).value
+        challenge['date'] = datetime.strptime(date, "%Y-%m-%d")
         challenge['count'] = int(self.bot_sheet.acell(NEXT_CHALLENGE_COUNT).value)
+
+        if challenge['date'] < datetime.now():
+            raise ValueError
         return challenge
 
     def get_leaderboard(self):
@@ -105,8 +88,21 @@ class OutreachSheet(object):
             leaderboard.append((name, count))
         return leaderboard
 
+    def get_all_members(self):
+        members = []
+        cells = self.bot_sheet.range("E4:I39")
+        rows = [cells[i:i+USER_INFO_ROW_LEN] for i in range(0, len(cells), USER_INFO_ROW_LEN)]
+        for row in rows:
+            member = {
+                'name': row[0].value,
+                'slack_id': row[1].value,
+                'total_donations': row[2].value,
+                'current_fortnight': row[4].value
+            }
+            if member['slack_id']:
+                members.append(member)
+        return members
 
-    # def report_error(self, message):
 
 
 class SignUpSheet(object):
@@ -174,41 +170,9 @@ class SignUpSheet(object):
             return False
 
 
-    # def mark_people_as_donated(self, people):
-    #     """Marks list of people as donated on sign up sheet
-
-    #     Batch version of mark_person_as_donated that bundles updates as one
-    #     api request for speed purposes. Will first attempt to use email key,
-    #     then first and last name
-
-    #     Args:
-    #         people (list): A list of dicts of people's info with keys:
-    #             first_name  (str): The first name of the person
-    #             last_name   (str): The last name of the person
-    #             email       (str): The email of the person
-
-    #     Returns:
-    #         A dict with list of people that were succesfully marked as donated,
-    #         and another list for those who could not be found in the sheet.
-
-    #         {
-    #             'success': [{'email': (str), 'first_name': (str),'last_name': (str)}],
-    #             'failed': [{'email': (str), 'first_name': (str),'last_name': (str)}]
-    #         }
-
-    #     Raises:
-    #     """
-    #     cell_list = []
-    #     for person in people:
-    #         pass
-
-
-
-
-
-
 if __name__ == "__main__":
-    # outreach = OutreachSheet()
-    # print outreach.get_individual_update_message("U3QD2RBN1")
-    signup = SignUpSheet()
-    print 'signup initialized'
+    outreach = OutreachSheet()
+    # print outreach.get_personal_update_messages()[0]['text']
+    print outreach.get_daily_update_message()
+    # signup = SignUpSheet()
+    # print 'signup initialized'
--- task.py
+++ task.py
@@ -1,14 +1,16 @@
 from modules import slack, sheets
 
-def create_update_message():
-    gs = sheets.OutreachSheet()
-    return gs.create_daily_update_message()
 
 def post_update_message():
     """Need to run tests on gs and bot before sending message"""
     bot = slack.SlackBot(slack.SLACK_TOKEN)
-    message = create_update_message()
-    bot.send_message(slack.GENERAL_CHANNEL_ID, message)
+
+    gs = sheets.OutreachSheet()
+    # update = gs.get_daily_update_message()
+    # bot.send_message(slack.GENERAL_CHANNEL_ID, update)
+
+    for message in gs.get_personal_update_messages():
+        bot.send_message(message['slack_id'], message['text'])
 
 if __name__ == "__main__":
     post_update_message()
